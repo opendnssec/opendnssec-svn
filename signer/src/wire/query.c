6dbec3a69b3806d8fc9f6a928dac7c4ab1a1adaa
Matthijs Mekking <matthijs@nlnetlabs.nl>
2011-11-14 14:39:33 +0000

    query error handling

--- signer/src/wire/query.c
+++ signer/src/wire/query.c
@@ -95,12 +95,204 @@ query_reset(query_type* q, size_t maxlen, int is_tcp)
 
 
 /**
+ * Error.
+ *
+ */
+static query_state
+query_error(query_type* q, ldns_pkt_rcode rcode)
+{
+    if (!q) {
+        return QUERY_DISCARDED;
+    }
+    buffer_clear(q->buffer);
+    buffer_pkt_set_qr(q->buffer);
+    buffer_pkt_set_rcode(q->buffer, rcode);
+    buffer_pkt_set_qdcount(q->buffer, 0);
+    buffer_pkt_set_ancount(q->buffer, 0);
+    buffer_pkt_set_nscount(q->buffer, 0);
+    buffer_pkt_set_arcount(q->buffer, 0);
+    buffer_set_position(q->buffer, BUFFER_PKT_HEADER_SIZE);
+    return QUERY_PROCESSED;
+}
+
+
+/**
+ * FORMERR.
+ *
+ */
+static query_state
+query_formerr(query_type* q)
+{
+    ldns_pkt_opcode opcode = LDNS_PACKET_QUERY;
+    if (!q) {
+        return QUERY_DISCARDED;
+    }
+    opcode = buffer_pkt_opcode(q->buffer);
+    /* preserve the RD flag, clear the rest */
+    buffer_pkt_set_flags(q->buffer, buffer_pkt_flags(q->buffer) & 0x0100U);
+    buffer_pkt_set_opcode(q->buffer, opcode);
+    ods_log_debug("[%s] formerr", query_str);
+    return query_error(q, LDNS_RCODE_FORMERR);
+}
+
+
+/**
+ * SERVFAIL.
+ *
+ */
+static query_state
+query_servfail(query_type* q)
+{
+    if (!q) {
+        return QUERY_DISCARDED;
+    }
+    ods_log_debug("[%s] servfail", query_str);
+    return query_error(q, LDNS_RCODE_SERVFAIL);
+}
+
+
+/**
+ * NOTIMPL.
+ *
+ */
+static query_state
+query_notimpl(query_type* q)
+{
+    if (!q) {
+        return QUERY_DISCARDED;
+    }
+    ods_log_debug("[%s] notimpl", query_str);
+    return query_error(q, LDNS_RCODE_NOTIMPL);
+}
+
+
+/**
+ * REFUSED.
+ *
+ */
+static query_state
+query_refused(query_type* q)
+{
+    if (!q) {
+        return QUERY_DISCARDED;
+    }
+    ods_log_debug("[%s] refused", query_str);
+    return query_error(q, LDNS_RCODE_REFUSED);
+}
+
+
+/**
+ * NOTIFY.
+ *
+ */
+static query_state
+query_process_notify(query_type* q, ldns_rr_type qtype)
+{
+    dnsin_type* dnsin = NULL;
+    if (!q || !q->zone) {
+        return QUERY_DISCARDED;
+    }
+    if (buffer_pkt_rcode(q->buffer) != LDNS_RCODE_NOERROR ||
+        buffer_pkt_qr(q->buffer) ||
+        !buffer_pkt_aa(q->buffer) ||
+        buffer_pkt_tc(q->buffer) ||
+        buffer_pkt_rd(q->buffer) ||
+        buffer_pkt_ra(q->buffer) ||
+        buffer_pkt_ad(q->buffer) ||
+        buffer_pkt_cd(q->buffer) ||
+        buffer_pkt_qdcount(q->buffer) != 1 ||
+        buffer_pkt_ancount(q->buffer) > 1 ||
+        buffer_pkt_nscount(q->buffer) != 0 ||
+        buffer_pkt_arcount(q->buffer) != 0 ||
+        qtype != LDNS_RR_TYPE_SOA) {
+        return query_formerr(q);
+    }
+    ods_log_assert(q->zone->name);
+    ods_log_debug("[%s] incoming notify for zone %s", query_str,
+        q->zone->name);
+    if (!q->zone->adinbound || q->zone->adinbound->type != ADAPTER_DNS) {
+        ods_log_error("[%s] zone %s is not configured to have input dns "
+            "adapter", query_str, q->zone->name);
+        return query_refused(q);
+    }
+    ods_log_assert(q->zone->adinbound->config);
+    dnsin = (dnsin_type*) q->zone->adinbound->config;
+    if (!acl_find(dnsin->allow_notify, &q->addr, NULL)) {
+        return query_refused(q);
+    }
+    /* get answer section and check inbound serial */
+    /* forward notify to xfrd */
+    buffer_pkt_set_qr(q->buffer);
+    return QUERY_PROCESSED;
+}
+
+
+/**
+ * QUERY.
+ *
+ */
+static query_state
+query_process_query(query_type* q, ldns_rr_type qtype)
+{
+    dnsout_type* dnsout = NULL;
+    if (!q || !q->zone) {
+        return QUERY_DISCARDED;
+    }
+    /* sanity checks */
+    if (buffer_pkt_qdcount(q->buffer) != 1 || buffer_pkt_tc(q->buffer)) {
+        buffer_pkt_set_flags(q->buffer, 0);
+        return query_formerr(q);
+    }
+    /* acl */
+    ods_log_assert(q->zone->name);
+    if (!q->zone->adoutbound || q->zone->adoutbound->type != ADAPTER_DNS) {
+        ods_log_error("[%s] zone %s is not configured to have output dns "
+            "adapter", query_str, q->zone->name);
+        return query_refused(q);
+    }
+    ods_log_assert(q->zone->adoutbound->config);
+    dnsout = (dnsout_type*) q->zone->adoutbound->config;
+    if (!acl_find(dnsout->provide_xfr, &q->addr, NULL)) {
+        buffer_pkt_set_qr(q->buffer);
+        return query_refused(q);
+    }
+    /* zone transfer? */
+    if (qtype == LDNS_RR_TYPE_AXFR || qtype == LDNS_RR_TYPE_IXFR) {
+        return query_notimpl(q);
+    }
+    /* (soa) query */
+    buffer_pkt_set_qr(q->buffer);
+    return QUERY_PROCESSED;
+}
+
+
+/**
+ * UPDATE.
+ *
+ */
+static query_state
+query_process_update(query_type* q)
+{
+    if (!q || !q->zone) {
+        return QUERY_DISCARDED;
+    }
+    ods_log_debug("[%s] dynamic update not implemented", query_str);
+    return query_notimpl(q);
+}
+
+
+/**
  * Process query.
  *
  */
 query_state
 query_process(query_type* q, void* engine)
 {
+    ldns_status status = LDNS_STATUS_OK;
+    ldns_pkt* pkt = NULL;
+    ldns_rr* rr = NULL;
+    ldns_pkt_opcode opcode = LDNS_PACKET_QUERY;
+    ldns_rr_type qtype = LDNS_RR_TYPE_SOA;
     engine_type* e = (engine_type*) engine;
     ods_log_assert(e);
     ods_log_assert(q);
@@ -118,29 +310,41 @@ query_process(query_type* q, void* engine)
         return QUERY_DISCARDED; /* not a query */
     }
     /* parse packet */
-
-/*
-    ldns_status status = LDNS_STATUS_OK;
-    status = ldns_wire2pkt(pkt, inbuf, (size_t)inlen);
+    status = ldns_wire2pkt(&pkt, buffer_current(q->buffer),
+        buffer_remaining(q->buffer));
     if (status != LDNS_STATUS_OK) {
-        ods_log_error("[%s] got bad packet: %s", sock_str,
+        ods_log_error("[%s] got bad packet: %s", query_str,
             ldns_get_errorstr_by_id(status));
-        return ODS_STATUS_ERR;
+        return query_formerr(q);
     }
-    *rr = ldns_rr_list_rr(ldns_pkt_question(*pkt), 0);
-    ods_log_assert(e);
+    rr = ldns_rr_list_rr(ldns_pkt_question(pkt), 0);
     lock_basic_lock(&e->zonelist->zl_lock);
-    *zone = zonelist_lookup_zone_by_dname(e->zonelist, ldns_rr_owner(*rr),
-        ldns_rr_get_class(*rr));
-    if (*zone && (*zone)->zl_status == ZONE_ZL_ADDED) {
-        *zone = NULL;
+    /* we can just lookup the zone, because we will only handle SOA queries,
+       zone transfers, updates and notifies */
+    q->zone = zonelist_lookup_zone_by_dname(e->zonelist, ldns_rr_owner(rr),
+        ldns_rr_get_class(rr));
+    /* don't answer for zones that are just added */
+    if (q->zone && q->zone->zl_status == ZONE_ZL_ADDED) {
+        q->zone = NULL;
     }
     lock_basic_unlock(&e->zonelist->zl_lock);
-    return ODS_STATUS_OK;
-*/
-
-    buffer_pkt_set_qr(q->buffer);
-    return QUERY_PROCESSED;
+    opcode = ldns_pkt_get_opcode(pkt);
+    qtype = ldns_rr_get_type(rr);
+    ldns_pkt_free(pkt);
+    if (!q->zone) {
+        return query_servfail(q);
+    }
+    switch(opcode) {
+        case LDNS_PACKET_NOTIFY:
+            return query_process_notify(q, qtype);
+        case LDNS_PACKET_QUERY:
+            return query_process_query(q, qtype);
+        case LDNS_PACKET_UPDATE:
+            return query_process_update(q);
+        default:
+            return query_notimpl(q);
+    }
+    return query_notimpl(q);
 }
 
 
